c3bbefd346dc4482b98300d826dbce721cd65864
Rekha8Thakur <rekha8thakur@users.noreply.github.com>
2026-08-05 12:09:52 +0530

    feat: slow down homepage client logos marquee scroll animation to 80s

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -72,7 +72,7 @@
         .animate-marquee {
             display: flex;
             width: max-content;
-            animation: marquee 50s linear infinite;
+            animation: marquee 80s linear infinite;
         }
         .animate-marquee:hover {
             animation-play-state: paused;
